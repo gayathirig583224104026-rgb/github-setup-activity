244cda8d886c4fd8edb1071106cc18ee5499ad50
gayathirig28 <gayathirig583224104026@nprcolleges.org>
2026-03-03 21:07:02 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+def display_details():
+    print("===== Student Portfolio System =====")
+    print("Name      : Gayathiri G")
+    print("Department: CSE")
+    print("Learning  : Git & GitHub")
+    print("Status    : Repository Created Successfully")
+
+display_details()
